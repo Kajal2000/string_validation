f590099f93c85eb11780394d5936c1d467846192
kajal <you@example.com>
2020-02-07 23:30:45 +0530

    task done

--- server.js
+++ server.js
@@ -1,37 +1,52 @@
-var readline = require('readline-sync');
-var name = readline.question(" enter your string :)");
-function isInnvalid(name){
-    var array = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","w","x","y","z"]
-    for (var i = 0; i < name.length; i++){
-        for (var j = 0; j < array.length; j++){
-            if (name[i] == array[j]){
-                var sto = ("valid h")
-            }
-            else{
-                return ("invalid h")
-            }
-        }
-    return (sto)
-    }
-}
-console.log(isInnvalid(name))
-
-
-
 // var readline = require('readline-sync');
 // var user = readline.question(" enter your string :)");
-// var user_input =user.split(",")
+// var user_input = user.split(",")
 // let string_data = " "
 // for (var i = 0; i < user_input.length; i++){
 //     for(var j = 0; j < user_input.length; j++){
-//         if (user_input[i] > user_input[j]){
-//             string_data = string_data + 1
-//             console.log("invalid h")
-//             break
-//         }
-//         else{
+//         if (user_input[i] == user_input[j]){
+//             // string_data = string_data + 1
 //             console.log("valid h")
 //             break
 //         }
+//         else{
+//             console.log("invalid h")
+//             break
+//         }
 //     }
+// }
+
+// var count = 0
+// var readline = require('readline-sync');
+// var user = readline.question(" enter your string :)");
+// for (var i = 0; i < user.length; i++){
+//     var letter = user[i]
+//     if(user.indexOf (letter)){
+//         count = count + 1
+//     }
+// }
+// if (count % 2 == 0){
+//     console.log("valid h")
+// }
+// else{
+//     console.log("not valid")
+// }
+
+
+
+
+// var count = 0
+// var readline = require('readline-sync');
+// var user = readline.question(" enter your string :)");
+// for (var i = 0; i < user.length; i++){
+//     var letter = user[i]
+//     if(user.indexOf (letter)){
+//     }
+//     count = count + 1
+// }
+// if (count % 2 == 0){
+//     console.log("valid h")
+// }
+// else{
+//     console.log("not valid")
 // }
--- string_valid.js
+++ string_valid.js
@@ -1,32 +1,32 @@
  
 const express = require('express');
 const server = express();
-server.use(express.json());
+server.use(express.urlencoded());
 var path = require("path")
 const port = 4001;
-server.use(express.urlencoded({extended:false}));
 
 server.get("/api", (req, res) => {
     res.sendFile(path.join(__dirname + '/string_valid.html'));
 });
+
 server.post("/api", (req, res) => {
-    var user = req.body.strings 
-    var user_input =user.split(",")
-    let str = " "
-    for (var i = 0; i <user_input.length; i++){
-        for(var j = 0; j<user_input.length; j++){
-            if (user_input[i] > user_input[j]){
-                str = str + 1
-                res.send("valid h")
-                break
-            }
-            else{
-                res.send("invalid h")
-                break
-            }
+    var count = 0
+    var user = req.body.user
+    let leng = user.length;
+    for (var i = 0; i < leng; i++){
+        var letter = user[i]
+        if(user.indexOf (letter)){
         }
+        count = count + 1
     }
-})
+    if (count % 2 == 0){
+        res.send("valid h")
+    }
+    else{
+        res.send("not valid")
+    }
+});
+
 server.listen(port, () => {
     console.log(`Server listening at ${port}`);
 })
